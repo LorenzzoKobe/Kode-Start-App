2d871483606b9d898c2019f646bacc8f085da15b
Lorenzzo Martins Patricio <lorenzzopatricio@Kobe-Apps-MacBook-Pro.local>
2025-10-31 15:27:38 -0300

    fix: Removendo packagedesnecessário no file recipe_grid_review.dart

--- chef_inteligente/lib/widgets/recipe_grid_view.dart
+++ chef_inteligente/lib/widgets/recipe_grid_view.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:cached_network_image/cached_network_image.dart';
 import '../models/spoonacular_item_model.dart'; 
 import '../providers/all_recipes_provider.dart';
 
